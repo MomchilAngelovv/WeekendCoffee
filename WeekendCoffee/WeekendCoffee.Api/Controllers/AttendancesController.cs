0a7f4f52e5a9a381c582dccc34d5b1fae17adc85
MomchilAngelov0040 <MomchilAngelov0040@gmail.com>
2022-11-13 16:45:19 +0200

    Some small changes.

--- WeekendCoffee/WeekendCoffee.Api/Controllers/AttendancesController.cs
+++ WeekendCoffee/WeekendCoffee.Api/Controllers/AttendancesController.cs
@@ -24,45 +24,7 @@
 			this.membersService = membersService;
 		}
 
-		[HttpGet]
-		public async Task<IActionResult> GetCurrentMeetingInformation()
-		{
-			var response = new GetCurrentMeetingInformationResponse();
-			var currentMeetingLabel = this.meetingsService.GenerateLabelAsync(false);
-			var currentMeeting = await meetingsService.GetByLabelAsync(currentMeetingLabel);
-			if (currentMeeting is null)
-			{
-				var currentSaturdayDate = DateTime.UtcNow;
-				while (currentSaturdayDate.DayOfWeek != DayOfWeek.Saturday)
-				{
-					currentSaturdayDate = currentSaturdayDate.AddDays(1);
-				}
-
-				currentMeeting = await this.meetingsService.CreateAsync(currentSaturdayDate);
-			}
-
-			var upcomingMeetingLabel = this.meetingsService.GenerateLabelAsync(true);
-			var upcomingMeeting = await meetingsService.GetByLabelAsync(upcomingMeetingLabel);
-			if (upcomingMeeting is null)
-			{
-				var upcomingMeetingDate = DateTime.UtcNow.AddDays(7);
-				while (upcomingMeetingDate.DayOfWeek != DayOfWeek.Saturday)
-				{
-					upcomingMeetingDate = upcomingMeetingDate.AddDays(1);
-				}
-
-				await this.meetingsService.CreateAsync(upcomingMeetingDate);
-			}
-
-			response.Label = currentMeeting.Label;
-
-			if (currentMeeting.Attendances is not null)
-			{
-				response.Members = currentMeeting.Attendances.Select(a => a.Member.NickName).ToList();
-			}
-
-			return this.Ok(response);
-		}
+		
 
 		[HttpPost]
 		public async Task<IActionResult> SignUpForMeeting(SignUpForMeetingRequestModel requestModel)
---
+++ WeekendCoffee/WeekendCoffee.Api/Controllers/MeetingsController.cs
@@ -0,0 +1,60 @@
+﻿namespace WeekendCoffee.Api.Controllers
+{
+	using Microsoft.AspNetCore.Mvc;
+
+	using WeekendCoffee.Services;
+	using WeekendCoffee.Api.Models.Responses;
+
+	[ApiController]
+	[Route("[controller]")]
+	public class MeetingsController : ControllerBase
+	{
+		private readonly IMeetingsService meetingsService;
+
+		public MeetingsController(
+			IMeetingsService meetingsService)
+		{
+			this.meetingsService = meetingsService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetCurrentMeetingInformation()
+		{
+			var response = new GetCurrentMeetingInformationResponse();
+			var currentMeetingLabel = this.meetingsService.GenerateLabelAsync(false);
+			var currentMeeting = await meetingsService.GetByLabelAsync(currentMeetingLabel);
+			if (currentMeeting is null)
+			{
+				var currentSaturdayDate = DateTime.UtcNow;
+				while (currentSaturdayDate.DayOfWeek != DayOfWeek.Saturday)
+				{
+					currentSaturdayDate = currentSaturdayDate.AddDays(1);
+				}
+
+				currentMeeting = await this.meetingsService.CreateAsync(currentSaturdayDate);
+			}
+
+			var upcomingMeetingLabel = this.meetingsService.GenerateLabelAsync(true);
+			var upcomingMeeting = await meetingsService.GetByLabelAsync(upcomingMeetingLabel);
+			if (upcomingMeeting is null)
+			{
+				var upcomingMeetingDate = DateTime.UtcNow.AddDays(7);
+				while (upcomingMeetingDate.DayOfWeek != DayOfWeek.Saturday)
+				{
+					upcomingMeetingDate = upcomingMeetingDate.AddDays(1);
+				}
+
+				await this.meetingsService.CreateAsync(upcomingMeetingDate);
+			}
+
+			response.Label = currentMeeting.Label;
+
+			if (currentMeeting.Attendances is not null)
+			{
+				response.Members = currentMeeting.Attendances.Select(a => a.Member.NickName).ToList();
+			}
+
+			return this.Ok(response);
+		}
+	}
+}
